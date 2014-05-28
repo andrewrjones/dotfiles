07eef819c56cedd0260cb7f4ac2ebe69604d37f8
Andrew Jones <andrew@arjones.co.uk>
2014-05-28 17:38:53 +0100

    add gitconfig

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles.bk              # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files="bashrc vimrc"
+files="bashrc vimrc gitconfig"
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
