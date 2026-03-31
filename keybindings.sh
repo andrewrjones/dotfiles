3398f5c1d806bdab5bbd8462e321e065a2425dd2
Andrew Jones <andrew@andrew-jones.com>
2026-03-31 12:53:38 +0100

    Make keybinding dir if not exists

--- keybindings.sh
+++ keybindings.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # https://jackbrewer.co.uk/mapping-a-hash-keyboard-button-in-osx
 
+mkdir -p ~/Library/KeyBindings
 ln -s ~/.dotfiles/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
